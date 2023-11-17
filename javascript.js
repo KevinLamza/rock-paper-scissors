7c5a9a878defa9376fd7236a4b3a296dcc14be73
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2023-11-17 11:35:14 +0100

    Add function getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,36 +1,55 @@
-/* 
-ROCK PAPER SCISSORS
 
-declare variable playerScore
-declare variable computerScore
+// ROCK PAPER SCISSORS
 
-init function getComputerChoice
-    declare variable computerChoice
-    chooses random number between 0, 1 and 2
-    assign result to computerchoice
-    assign a string of rock paper scissors to computerChoice
-    return computerChoice
+// declare variable playerScore
+let playerScore = 0;
 
-init function getPlayerChoice
-    declare variable for playerChoice
-    ask player for choice
-    assign value to playerChoice
-    change string into lowercase only
-    return playerChoice
+// declare variable computerScore
+let computerScore = 0;
 
-init function singleRound (computerChoice, playerChoice)
-    variable to save winningannouncement
-    if .. to check playerChoice
-        nested if .. for computerChoice
-        add a point to either playerScore or computerScore
-    return string with winning announcement
+// init function getComputerChoice
+function getComputerChoice() {
+    // declare variable computerChoice
+    let computerChoice;
 
-init function game 
-    for loop 5 mal
-        getchoices
-        playsingleround
-        print string for winner of current round and current score
+    // chooses random number between 0, 1 and 2
+    // assign result to computerchoice
+    computerChoice = Math.floor(Math.random() * 3);
 
-game()
-compare winning conditions
-*/
+    // assign a string of rock paper scissors to computerChoice
+    if computerChoice === 0 {
+        computerChoice = "rock";
+    } else if computerChoice === 1 {
+        computerChoice = "paper";
+    } else if computerChoice === 2 {
+        computerChoice = "scissors";
+    } else {
+        computerChoice = null;
+    }
+    
+    // return computerChoice
+    return computerChoice;
+}
+
+// init function getPlayerChoice
+//     declare variable for playerChoice
+//     ask player for choice
+//     assign value to playerChoice
+//     change string into lowercase only
+//     return playerChoice
+
+// init function singleRound (computerChoice, playerChoice)
+//     variable to save winningannouncement
+//     if .. to check playerChoice
+//         nested if .. for computerChoice
+//         add a point to either playerScore or computerScore
+//     return string with winning announcement
+
+// init function game 
+//     for loop 5 mal
+//         getchoices
+//         playsingleround
+//         print string for winner of current round and current score
+
+// game()
+// compare winning conditions
